6084bb100d115944281e73e9b55257a58fd1e02f
alexcook190-dot <alexcook190@gmail.com>
2026-09-25 15:10:08 -0400

    Update measurement_circuits.py

--- measurement_circuits.py
+++ measurement_circuits.py
@@ -15,24 +15,24 @@ def QST(qc):
   noise_model = NoiseModel()
 
   for gate in ["x", "y", "z", "h", "rx", "ry", "rz"]:
-    depolarizing_error_gate = depolarizing_error(0.05, 1)
+    depolarizing_error_gate = depolarizing_error(0.05, 5)
     for q in range(n_qubits):
         noise_model.add_quantum_error(depolarizing_error_gate, [gate], [q], warnings=False)
 
     total_counts = {}
     for i in range(max_iterations_step_1):
     qc_transpiled = transpile(qc)
-    res = aer_simulator.run(qc_transpiled, noise_model=noise_model, shots=1)
+    res = aer_simulator.run(qc_transpiled, noise_model=noise_model, shots=5)
 
     counts = res.result().get_counts()
-    # { "0": 0, "1": 1 }
+     { "00": 00, "01": 01, "10": 10, "11": 11 }
   # find the lowest values in total_counts and add them to a filter
   
   return correct_state_estimate
 
-correct_state_true = ...
+correct_state_true = Max(counts)
 correct_state_estimate = QST(qc)
-
 # 0 <= F <= 1
 fidelity = state_fidelity(correct_state_true, correct_state_estimate)
+println(fidelity)
   
